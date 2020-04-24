fd42db7fe2d29d2196e89f7bf3040ab38a35e135
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-24 09:52:30 -0700

    fetched the data and the login works

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,53 +1,67 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utills/axiosWithAuth'
 
-class Login extends React.Component {
+const Login = props => {
 
-  state = {
-    credentials: {
-      username: "",
-      password: "",
-    }
+  const [user, setUser] = useState({
+
+    username: '',
+    password: '',
+
   }
+  )
+console.log(user)
+  const handleChange = event => {
+    setUser({
 
-  handleChange = event =>{
-    this.setState({
-        credentials: {
-            ...this.state.credentials,
-            [event.target.name]: event.target.value 
-        }
+      ...user,
+      [event.target.name]: event.target.value
+
     })
-}
-
-  render() {
-    return (
-      <>
-        <h1>Welcome to the Bubble App!</h1>
-        <div className='formDiv'>
-          
-          
-          <form>
-          <label htmlFor='username' name='username' >Username*</label>
-            <input
-              type='text'
-              name='username'
-              value={this.state.credentials.username}
-              onChange= {this.handleChange}
-            />
-            
-            <label htmlFor='password' name='password' >Password*</label>
-            
-            <input
-            type= 'password'
-            name= 'password'
-            value={this.state.credentials.password}
-            onChange= {this.handleChange}
-            />
-          </form>
-          <button>Log In</button>
-        </div>
-      </>
-    );
   }
+
+  const handleLogin = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post('/api/login', user)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubbles')
+      })
+      .catch(err => console.log('Login Error:', err))
+  }
+
+
+  return (
+    <>
+      <h1>Welcome to the Bubble App!</h1>
+      <div className='formDiv'>
+
+
+        <form onSubmit={handleLogin}>
+          <label htmlFor='username' name='username' >Username*</label>
+          <input
+            type='text'
+            name='username'
+            value={user.username}
+            onChange={handleChange}
+          />
+
+          <label htmlFor='password' name='password' >Password*</label>
+
+          <input
+            type='password'
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+          />
+          <button >Log In</button>
+        </form>
+        
+      </div>
+    </>
+  );
+
 };
 
 export default Login;
