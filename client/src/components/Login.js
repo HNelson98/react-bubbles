d67bf67f8b0040afa4f9b833c8553711f88a42a4
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-24 08:21:40 -0700

    added on change and a button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,29 +9,41 @@ class Login extends React.Component {
     }
   }
 
+  handleChange = event =>{
+    this.setState({
+        credentials: {
+            ...this.state.credentials,
+            [event.target.name]: event.target.value 
+        }
+    })
+}
 
   render() {
     return (
       <>
         <h1>Welcome to the Bubble App!</h1>
         <div className='formDiv'>
-          <br/>
-          <label htmlFor='username' name='username' >Username*</label>
+          
+          
           <form>
+          <label htmlFor='username' name='username' >Username*</label>
             <input
               type='text'
               name='username'
               value={this.state.credentials.username}
+              onChange= {this.handleChange}
             />
-            <br />
+            
             <label htmlFor='password' name='password' >Password*</label>
-            <br />
+            
             <input
             type= 'password'
             name= 'password'
             value={this.state.credentials.password}
+            onChange= {this.handleChange}
             />
           </form>
+          <button>Log In</button>
         </div>
       </>
     );
